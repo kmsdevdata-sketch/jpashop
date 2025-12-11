1776f85a2c33d10c6d7d8139204366f38251152a
kmsdevdata-sketch <kmsdevdata@gmail.com>
2025-12-11 16:47:09 +0900

    database connection test

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,4 +1,4 @@
-package jpabokk.jpashop;
+package jpabook.jpashop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpashopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpashopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JpashopApplication.class, args);
+
+
+    }
 
 }
